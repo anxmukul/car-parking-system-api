fe971fd4230378f8b30aab0b5e4189c815d89ca0
anxmukul <kmukul1197@gmail.com>
2022-11-17 22:54:54 +0530

    Adding method to get all reserved parking slot

--- src/parkinglot/parkinglot/parkinglot.controller.ts
+++ src/parkinglot/parkinglot/parkinglot.controller.ts
@@ -11,19 +11,29 @@ class Noofslotrequest {
 }
 
 class carDetail {
-    car_reg_no: string;
-    car_color: string;
+  car_reg_no: string;
+  car_color: string;
 }
 
 class allocatedSlot {
-    allocated_slot_number: number;
+  allocated_slot_number: number;
+}
+class badresponse {
+    Error: string;
 }
 class FreeSlotResponse {
-    freed_slot_number: number;
+  freed_slot_number: number;
 }
-class FreeSlotRequest{
-    slot_number: number;
+class FreeSlotRequest {
+  slot_number: number;
 }
+
+class ReservedSlotResponse {
+  slot_no: number;
+  registration_no: string;
+  color: string;
+}
+
 // class CarRegNos{
 //     cars: Array<string>
 // }
@@ -32,7 +42,6 @@ class FreeSlotRequest{
 // }
 @Controller('parkinglot')
 export class ParkinglotController {
-
   @Post()
   createSlots(@Body() reqestedSlot: Noofslotrequest): Slotresponse {
     let ns = slotarrayobj.intiliazeSlot(reqestedSlot.no_of_slot);
@@ -51,29 +60,34 @@ export class ParkinglotController {
   }
 
   @Post('park')
-  park(@Body() newCar: carDetail): allocatedSlot {
+  park(@Body() newCar: carDetail): allocatedSlot | badresponse{
     let slotGiven = slotarrayobj.parkCar(newCar.car_reg_no, newCar.car_color);
     console.log('Slots allocated', slotGiven);
-    if(slotGiven != 0){
-        const returnobj: allocatedSlot = new allocatedSlot();
-        returnobj.allocated_slot_number = slotGiven;
-        return returnobj;
+    if (slotGiven != 0) {
+      const returnobj: allocatedSlot = new allocatedSlot();
+      returnobj.allocated_slot_number = slotGiven;
+      return returnobj;
     }
+    else{
+        const returnobj: badresponse = new badresponse();
+        returnobj.Error = "Parking lot is Full!";
+    }
+    
   }
 
   @Get('registration_numbers/:color')
-  findParticularColorCar(@Param('color') color: string): string[]{
+  findParticularColorCar(@Param('color') color: string): string[] {
     let car_no = slotarrayobj.findCarByColor(color);
     return car_no;
   }
 
-//   @Get('registration_numbers/:color')
-//   findParticularColorCar(@Param('color') color: string): CarRegNos{
-//     let car_no = slotarrayobj.findCarByColor(color);
-//     const responsearr: CarRegNos = new CarRegNos();
-//     responsearr.cars = car_no;
-//     return responsearr;
-//   }
+  //   @Get('registration_numbers/:color')
+  //   findParticularColorCar(@Param('color') color: string): CarRegNos{
+  //     let car_no = slotarrayobj.findCarByColor(color);
+  //     const responsearr: CarRegNos = new CarRegNos();
+  //     responsearr.cars = car_no;
+  //     return responsearr;
+  //   }
 
   @Get('slot_numbers/:color')
   findSlotOfCarByColor(@Param('color') color: string): string[] {
@@ -84,8 +98,16 @@ export class ParkinglotController {
   @Post('clear')
   Free(@Body() free_slot_no: FreeSlotRequest): FreeSlotResponse {
     let freed_slot = slotarrayobj.freeSlot(free_slot_no.slot_number);
-    const returnobj: FreeSlotResponse= new FreeSlotResponse();
+    const returnobj: FreeSlotResponse = new FreeSlotResponse();
     returnobj.freed_slot_number = freed_slot;
     return returnobj;
+  }
+
+  @Get('status')
+  findReservedSlot(): ReservedSlotResponse[] {
+    let reserved_slots_array = slotarrayobj.getReservedSlot();
+    // const reservedSlots: ReservedSlotResponse = new ReservedSlotResponse();
+    // reservedSlots.registration_no = reserved_slots_array.;
+    return reserved_slots_array;
   }
 }
--- src/parkinglot/parkinglot/slot.dto.ts
+++ src/parkinglot/parkinglot/slot.dto.ts
@@ -5,11 +5,17 @@ class SlotDTO {
   status: 'reserved' | 'unreserved';
 }
 
+class ReservedSlotResponse {
+  slot_no: number;
+  registration_no: string;
+  color: string;
+}
+
 class Slotarray {
   slots: SlotDTO[];
-
+  reservedslots: ReservedSlotResponse[];
   public intiliazeSlot(no_of_slot: number): number {
-    this.slots = []
+    this.slots = [];
     for (let i = 1; i <= no_of_slot; i++) {
       const temp: SlotDTO = new SlotDTO();
       temp.id = i;
@@ -22,10 +28,10 @@ class Slotarray {
   public addnewSlot(new_slot: number): number {
     var total_no_slots = this.slots.length;
     for (let i = 1; i <= new_slot; i++) {
-        const temp: SlotDTO = new SlotDTO();
-        temp.id = i+total_no_slots;
-        temp.status = 'unreserved';
-        this.slots.push(temp);
+      const temp: SlotDTO = new SlotDTO();
+      temp.id = i + total_no_slots;
+      temp.status = 'unreserved';
+      this.slots.push(temp);
     }
     var total_no_slots = this.slots.length;
     return total_no_slots;
@@ -33,29 +39,29 @@ class Slotarray {
 
   public parkCar(reg_no: string, color: string): number {
     var total_no_slots = this.slots.length;
-    let allocate_slot;
-    for(let i=1; i<=total_no_slots; i++){
-        if(this.slots[i].status == 'unreserved'){
-            allocate_slot = i;
-            this.slots[i].car_reg_no = reg_no;
-            this.slots[i].car_color = color;            ////Isme err throw karna h agar khali nhi hua to ....
-            this.slots[i].status = 'reserved';
-            return allocate_slot;
-        }
+    let allocated_slot;
+    for (let i = 0; i <total_no_slots; i++) {
+      if (this.slots[i].status == 'unreserved') {
+        allocated_slot = i+1;
+        this.slots[i].car_reg_no = reg_no;
+        this.slots[i].car_color = color; ////Isme err throw karna h agar khali nhi hua to ....
+        this.slots[i].status = 'reserved';
+        return allocated_slot;
+      }
     }
     return 0;
   }
 
   public findCarByColor(color: string): string[] {
-    let car_registration_number: Array<string>
+    let car_registration_number: Array<string>;
     car_registration_number = [];
     // console.log('Looking for color: ', color);
     var total_no_slots = this.slots.length;
-    for(let i=0; i<total_no_slots; i++){
-        if(this.slots[i].car_color === color){
-            // console.log('Inside for loop');
-            car_registration_number.push(this.slots[i].car_reg_no);
-        }
+    for (let i = 0; i < total_no_slots; i++) {
+      if (this.slots[i].car_color === color) {
+        // console.log('Inside for loop');
+        car_registration_number.push(this.slots[i].car_reg_no);
+      }
     }
     // console.log(car_registration_number.length);
     // console.log('Array', car_registration_number);
@@ -63,26 +69,44 @@ class Slotarray {
   }
 
   public findSlotOfCarByColor(color: string): string[] {
-    let slot_id: Array<string>
-    slot_id= [];
+    let slot_id: Array<string>;
+    slot_id = [];
     // console.log('Looking for color: ', color);
     var total_no_slots = this.slots.length;
-    for(let i=0; i<total_no_slots; i++){
-        if(this.slots[i].car_color === color){
-            // console.log('Inside for loop');
-            let str = String(this.slots[i].id);
-            slot_id.push(str);
-        }
+    for (let i = 0; i < total_no_slots; i++) {
+      if (this.slots[i].car_color === color) {
+        // console.log('Inside for loop');
+        let str = String(this.slots[i].id);
+        slot_id.push(str);
+      }
     }
     return slot_id;
   }
 
-  public freeSlot(slot_num:number): number{
+  public freeSlot(slot_num: number): number {
     console.log('Slot number to delete', slot_num);
-    this.slots[slot_num].car_reg_no = null;
-    this.slots[slot_num].car_color = null;
-    this.slots[slot_num].status = "unreserved";
+    console.log('Id of slot_no to be deleted:', this.slots[slot_num].id);
+    this.slots[slot_num - 1].car_reg_no = null;
+    this.slots[slot_num - 1].car_color = null;
+    this.slots[slot_num - 1].status = 'unreserved';
     return slot_num;
+  }
+
+  public getReservedSlot(): ReservedSlotResponse[] {
+    this.reservedslots = [];
+    var total_no_slots = this.slots.length;
+    for (let i = 0; i < total_no_slots; i++) {
+      if (this.slots[i].status === 'reserved') {
+        console.log('Inside for loop', );
+        const temp: ReservedSlotResponse = new ReservedSlotResponse();
+        temp.color = this.slots[i].car_color;
+        temp.registration_no = this.slots[i].car_reg_no;
+        temp.slot_no = this.slots[i].id;
+        console.log(temp);
+        this.reservedslots.push(temp);
+      }
+    }
+    return this.reservedslots;
   }
 }
 
